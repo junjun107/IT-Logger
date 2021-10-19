5cb9bd26c78626bcfb1ec9922948317257265ffe
unknown <kate86107@gmail.com>
2021-10-19 12:57:17 -0700

    redux store & provider setup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   useEffect(() => {
@@ -16,18 +18,20 @@ function App() {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
 
-      <div className='container'>
-        <Addbtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+        <div className='container'>
+          <Addbtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
